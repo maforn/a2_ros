4ccd96bc11245a160cd6e0d0e47cdadf640e0819
pau.reverte <pau.reverte@eurecat.org>
2026-06-23 15:33:38 +0200

    Minor incluide

--- src/meta_packages/a2_ros/launch/nuc.launch.py
+++ src/meta_packages/a2_ros/launch/nuc.launch.py
@@ -23,8 +23,9 @@ Usage:
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
